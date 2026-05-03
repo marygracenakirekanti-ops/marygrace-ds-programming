453a9dd85139e4805b7ef6cd4f416ab23ab14769
marygracenakirekanti-ops <marygracenakirekanti@gmail.com>
2026-05-03 18:37:50 +0530

    Add files via upload

---
+++ copy.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+struct student
+{
+	char name[50];
+	int marks;
+};
+int main()
+{
+	struct student s1,s2;
+	strcpy(s1.name,"pavani");
+	s1.marks=90;
+	s2=s1;
+	printf("name:%s,marks:%d\n",s2.name,s2.marks);
+	return 0;
+}
